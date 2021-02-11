fd54d49c28ab47d960e4facdeab6243500331a9b
rebeccatimoteo <rebeccatimoteo@artist.github.com>
2021-02-11 07:26:49 +0100

    newchanges

--- js/site.js
+++ js/site.js
@@ -11,6 +11,31 @@ var btnContact = document.querySelector('.rt-btn-contact');
 // });
 
 //Abrindo e Fechando Informaçoes de contato
+
+if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 120;
+    else if (event.detail) delta = -event.detail / 3;
+
+    handle(delta);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+
+function handle(delta) {
+    var time = 1000;
+    var distance = 300;
+
+    $('html, body').stop().animate({
+        scrollTop: $(window).scrollTop() - (distance * delta)
+    }, time);
+}
+
+
+
 btnContact.addEventListener('click', function() {
     var boxContact = document.querySelector('.rt-contact-info');
     boxContact.classList.toggle('rt-is-open');
