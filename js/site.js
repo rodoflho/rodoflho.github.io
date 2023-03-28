fdf1353d18b1e34c0c33ea94916837563a259ec7
rebeccatimoteo <48012788+rebeccatimoteo@users.noreply.github.com>
2023-03-29 01:14:17 +0200

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,19 +1,3 @@
-var btnContact = document.querySelector('.rt-btn-contact');
-
-//Page Preloader
-// window.addEventListener('load', function() {
-//     var pagePreloder = document.querySelector('.rt-preloader');
-//     pagePreloder.classList.add('rt-fade-out');
-
-//     setTimeout(function() {
-//         pagePreloder.style.display = 'none';
-//     }, 2000);
-// });
-
-
-//mouse scroll
-
-
 
 
 if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
@@ -38,13 +22,106 @@ function handle(delta) {
     }, time);
 }
 
-//end mouuse scroll
+function myFunction(x) {
+           if (x.matches) { // If media query matches
+              document.body.style.backgroundColor = "yellow";
+              
+           } else {
+             document.body.style.backgroundColor = "pink";
+         }
+        }
+          
+         var x = window.matchMedia("(max-width: 768px)")
+        myFunction(x) // Call listener function at run time
+         x.addListener(myFunction) // Attach listener function on state changes
 
-btnContact.addEventListener('click', function() {
-    var boxContact = document.querySelector('.rt-contact-info');
-    boxContact.classList.toggle('rt-is-open');
-    this.classList.toggle('rt-change-icon');
-});
+
+function createCounter() {
+    let count = 0;
+    return function(inc) {
+        if (inc == 1) {
+            count++;      
+        }else if (inc==0){
+            count=0;
+        }
+        return count;
+    }
+}
+const counter = createCounter();
+const modal_counter = createCounter();
+
+
+function splitStatue() {
+    counter(inc=1)
+    {
+            $("#box1").animate({
+                bottom: "18%",
+                right: "-50%"
+            }, 1200);
+            $("#box2").animate({
+                bottom: "25%",
+                right: "-55%"
+            }, 1200);
+            $("#box3").animate({
+                bottom: "30%",
+                right: "-65%"
+            }, 1200);
+            $("#box4").animate({
+                bottom: "45%",
+                right: "-50%"
+            }, 1200);
+            $("#box5").animate({
+                bottom: "55%",
+                right: "-60%"
+            }, 1200);
+        }
+}
+
+function resetStatue() {
+    counter(inc=0)
+    {
+        $("#box1").animate({
+            bottom: "0%",
+            right: "-55%"
+        }, 400);
+        $("#box2").animate({
+            bottom: "150px",
+            right: "-55%"
+        }, 1200);
+        $("#box3").animate({
+            bottom: "266px",
+            right: "-55%"
+        }, 400);
+        $("#box4").animate({
+            bottom: "422px",
+            right: "-55%"
+        }, 400);
+        $("#box5").animate({
+            bottom: "593px",
+            right: "-56%"
+        }, 400);
+    }
+}
+
+
+function addClickHandlerForModal(elem, arg1, mobile_phone) {
+    elem.addEventListener('click', function(e) {
+        condition = true
+        if (mobile_phone == true){
+            condition = counter() > 0
+        }
+        if (condition) {
+            e.stopPropagation();
+            mymodal = '#myModal'+arg1
+
+            $(mymodal).modal('show')
+
+            counter(inc=1)
+            modal_counter(inc=1)
+        }
+    }, false);
+}
+
 
 $(document).ready(function() {
 
@@ -53,207 +130,95 @@ $(document).ready(function() {
     $("#box8").hide();
     $("#box9").hide();
     $("#box10").hide();
+
+
     //  $( '#gif1').attr('src','https://i1.sndcdn.com/artworks-000254919542-6kdx39-t500x500.jpg');
 
 
-    //Selecting each one separately
-    $("#box1").hover(function() {
-        $("#box1").animate({
-            opacity: 0.4
-        }, 300);
+    const splitEvent = e => { // Pass event (e) argument into function
+        e.stopPropagation(); // Stop the document click event from running
+        if (counter() == 0) {
+            splitStatue()
+        }
+        //document.getElementById('inc').value = counter()
+    }
 
-        $("#box6").show();
-        $("#box6").animate({
-            bottom: "1260px",
-            right: "300px"
-        }, 800);
+    const resetEvent = () => {
+      if (modal_counter() == 0) {
+        resetStatue()
+      }
+    }
+    
 
-    }, function() {
-        $("#box1").animate({
-            opacity: 1
-        }, 300);
+    // Listen for click event
+    const button = document.getElementById("dragZone");
+    const is_mobile_phone = document.body.style.backgroundColor == "yellow"
+    if (is_mobile_phone){ // if mobile phone
+        button.addEventListener('click', splitEvent);
+        document.addEventListener('click', resetEvent) // Add second event listener on the document
 
-        $("#box6").animate({
-            bottom: "1100px",
-            right: "10px"
-        }, 800);
-        $("#box6").hide();
-        $('#gif1').attr('src', 'img/box1.gif');
-
-
-    });
-    $("#box2").hover(function() {
-        $("#box2").animate({
-            opacity: 0.4
-        }, 300);
-
-        $("#box7").show();
-        $("#box7").animate({
-            bottom: "1100px",
-            right: "200px"
-        }, 800);
-
-    }, function() {
-        $("#box2").animate({
-            opacity: 1
-        }, 300);
-
-        $("#box7").animate({
-            bottom: "1000px",
-            right: "-100px"
-        }, 800);
-        $("#box7").hide();
-        $('#gif2').attr('src', 'img/box2.gif');
-
-
-    });
-    $("#box3").hover(function() {
-        $("#box3").animate({
-            opacity: 0.4
-        }, 300);
-
-
-        $("#box8").show();
-        $("#box8").animate({
-            bottom: "980px",
-            right: "200px"
-        }, 800);
-
-
-    }, function() {
-        $("#box3").animate({
-            opacity: 1
-        }, 300);
-
-        $("#box8").animate({
-            bottom: "860px",
-            right: "-100px"
-        }, 800);
-        $("#box8").hide();
-        $('#gif3').attr('src', 'img/box3.gif');
-
-
-
-    });
-    $("#box4").hover(function() {
-        $("#box4").animate({
-            opacity: 0.4
-        }, 300);
-
-        $("#box9").show();
-        $("#box9").animate({
-            bottom: "770px",
-            right: "200px"
-        }, 800);
-
-
-    }, function() {
-        $("#box4").animate({
-            opacity: 1
-        }, 300);
-
-        $("#box9").animate({
-            bottom: "710px",
-            right: "-100px"
-        }, 800);
-        $("#box9").hide();
-        $('#gif4').attr('src', 'img/box4.gif');
-
-
-    });
-    $("#box5").hover(function() {
-        $("#box5").animate({
-            opacity: 0.4
-        }, 300);
-
-        $("#box10").show();
-        $("#box10").animate({
-            bottom: "600px",
-            right: "260px"
-        }, 800);
-
-
-    }, function() {
-        $("#box5").animate({
-            opacity: 1
-        }, 300);
-
-        $("#box10").animate({
-            bottom: "550px",
-            right: "-110px"
-        }, 800);
-        $("#box10").hide();
-        $('#gif5').attr('src', 'img/box5.gif');
-
-    });
-
-
-    //splitting all
-    $('#dragZone').hover(function() {
+        button.addEventListener('touchstart', splitEvent);
+        document.addEventListener('touchstart', resetEvent);
+    
+    }else{
+        $('#dragZone').hover(function() {
             $("#box1").animate({
-                bottom: "130px",
-                right: "160px"
-            }, 600);
+                bottom: "18%",
+                right: "-50%"
+            }, 1200);
             $("#box2").animate({
-                bottom: "240px",
-                right: "-150px"
-            }, 600);
+                bottom: "25%",
+                right: "-55%"
+            }, 1200);
             $("#box3").animate({
-                bottom: "350px",
-                right: "60px"
-            }, 600);
+                bottom: "30%",
+                right: "-65%"
+            }, 1200);
             $("#box4").animate({
-                bottom: "450px",
-                right: "-140px"
-            }, 600);
+                bottom: "45%",
+                right: "-50%"
+            }, 1200);
             $("#box5").animate({
-                bottom: "550px",
-                right: "160px"
-            }, 600);
+                bottom: "55%",
+                right: "-60%"
+            }, 1200);
         },
         function() {
             $("#box1").animate({
-                bottom: "0px",
-                right: "0px"
+                bottom: "0%",
+                right: "-55%"
             }, 400);
             $("#box2").animate({
                 bottom: "150px",
-                right: "0px"
-            }, 400);
+                right: "-55%"
+            }, 1200);
             $("#box3").animate({
                 bottom: "266px",
-                right: "0px"
+                right: "-55%"
             }, 400);
             $("#box4").animate({
                 bottom: "422px",
-                right: "0px"
+                right: "-55%"
             }, 400);
             $("#box5").animate({
                 bottom: "593px",
-                right: "-11px"
+                right: "-56%"
             }, 400);
         });
+    }
+
+    for (let i = 1; i < 6; i++) {
+        addClickHandlerForModal(document.getElementById(`box${i}`), i.toString(), is_mobile_phone)
+      }
 
 
 
-
+        
 
     //click event separately
-    $('#box1').click(function() {
-        $('#myModal1').modal('show');
-    });
-    $('#box2').click(function() {
-        $('#myModal2').modal('show');
-    });
-    $('#box3').click(function() {
-        $('#myModal3').modal('show');
-    });
-    $('#box4').click(function() {
-        $('#myModal4').modal('show');
-    });
-    $('#box5').click(function() {
-        $('#myModal5').modal('show');
-    });
+    ///*
+
+    //*/
 
 
 });
