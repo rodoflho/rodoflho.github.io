ecb1689076f837c363341cf25ce080a54183ea05
rebeccatimoteo <48012788+rebeccatimoteo@users.noreply.github.com>
2023-03-30 16:34:50 +0200

    Update site.js

--- js/site.js
+++ js/site.js
@@ -22,7 +22,7 @@ function handle(delta) {
     }, time);
 }
 
-function myFunction(x) {
+/*function myFunction(x) {
            if (x.matches) { // If media query matches
               document.body.style.backgroundColor = "yellow";
               
@@ -34,7 +34,7 @@ function myFunction(x) {
          var x = window.matchMedia("(max-width: 768px)")
         myFunction(x) // Call listener function at run time
          x.addListener(myFunction) // Attach listener function on state changes
-
+*/
 
 function createCounter() {
     let count = 0;
